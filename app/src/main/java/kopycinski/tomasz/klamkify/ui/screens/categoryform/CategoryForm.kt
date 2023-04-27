512d56d890c40b233995cf52ee639cbf4161c71a
Tomasz Kopyciński <mrnexiol@gmail.com>
2023-04-27 07:34:35 +0200

    Added top bar to CategoryForm

--- app/src/main/java/kopycinski/tomasz/klamkify/ui/screens/categoryform/CategoryForm.kt
+++ app/src/main/java/kopycinski/tomasz/klamkify/ui/screens/categoryform/CategoryForm.kt
@@ -5,12 +5,16 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import kopycinski.tomasz.klamkify.R
 
 @Composable
 fun CategoryForm(
@@ -24,19 +28,30 @@ fun CategoryForm(
         }
     }
 
-    Column(
-        Modifier.padding(8.dp)
-    ) {
-        OutlinedTextField(
-            modifier = Modifier
-                .padding(bottom = 8.dp)
-                .fillMaxWidth(),
-            value = viewModel.categoryName.value,
-            onValueChange = { viewModel.update(it) },
-            label = { Text(text = "Category name") }
-        )
-        Button(onClick = { viewModel.save(onSuccessSave) }) {
-            Text(text = "Save")
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.add_category)) }
+            )
         }
+    ) {paddingValues ->
+        Column(
+            Modifier
+                .padding(paddingValues)
+                .padding(8.dp)
+        ) {
+            OutlinedTextField(
+                modifier = Modifier
+                    .padding(bottom = 8.dp)
+                    .fillMaxWidth(),
+                value = viewModel.categoryName.value,
+                onValueChange = { viewModel.update(it) },
+                label = { Text(text = stringResource(id = R.string.name)) }
+            )
+            Button(onClick = { viewModel.save(onSuccessSave) }) {
+                Text(text = stringResource(id = R.string.save))
+            }
+        }
+
     }
 }
