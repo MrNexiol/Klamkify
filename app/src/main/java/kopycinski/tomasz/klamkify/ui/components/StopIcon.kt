f3b1d6e5a4d27a1c8c412447a4821da4e2df4bf4
Tomasz Kopyciński <mrnexiol@gmail.com>
2023-05-10 19:58:12 +0200

    Reordered gradle dependencies Modified Stop icon

--- app/src/main/java/kopycinski/tomasz/klamkify/ui/components/StopIcon.kt
+++ app/src/main/java/kopycinski/tomasz/klamkify/ui/components/StopIcon.kt
@@ -5,22 +5,14 @@ import androidx.compose.material.icons.materialIcon
 import androidx.compose.material.icons.materialPath
 import androidx.compose.ui.graphics.vector.ImageVector
 
-@Suppress("UnusedReceiverParameter")
-val Icons.Filled.Stop: ImageVector
-    get() {
-        if (_stop != null) {
-            return _stop!!
+val Icons.Filled.Stop: ImageVector by lazy {
+    materialIcon(name = "Filled.Stop") {
+        materialPath {
+            moveTo(6.0f, 5.0f)
+            verticalLineToRelative(14F)
+            horizontalLineToRelative(14F)
+            verticalLineTo(5F)
+            close()
         }
-        _stop = materialIcon(name = "Filled.Stop") {
-            materialPath {
-                moveTo(6.0f, 5.0f)
-                verticalLineToRelative(14F)
-                horizontalLineToRelative(14F)
-                verticalLineTo(5F)
-                close()
-            }
-        }
-        return _stop!!
     }
-
-private var _stop: ImageVector? = null
+}
